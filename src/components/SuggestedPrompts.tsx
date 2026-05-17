ccf811b2ad481669a51c444ee7225af383a8f5c0
Mark Shperkin <markshperkin1@gmail.com>
2026-05-17 09:47:48 +0300

    feat: add swim and work ethic suggestion prompts

--- src/components/SuggestedPrompts.tsx
+++ src/components/SuggestedPrompts.tsx
@@ -4,6 +4,8 @@ const PROMPTS = [
   "Tell me about Mark's Thesis",
   'How do I reach Mark?',
   'Does Mark know how to play backgammon?',
+  'Does Mark know how to swim?',
+  "Tell me about Mark's work ethic.",
 ]
 
 type Props = {
