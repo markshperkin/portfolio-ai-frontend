680bcbdfa491ac60b971891c10fbd497e597036e
Mark Shperkin <markshperkin1@gmail.com>
2026-05-17 12:41:32 +0300

    feat: add career goals suggestion prompt

--- src/components/SuggestedPrompts.tsx
+++ src/components/SuggestedPrompts.tsx
@@ -6,6 +6,7 @@ const PROMPTS = [
   'Does Mark know how to play backgammon?',
   'Does Mark know how to swim?',
   "Tell me about Mark's work ethic.",
+  "What are Mark's career goals?",
 ]
 
 type Props = {
